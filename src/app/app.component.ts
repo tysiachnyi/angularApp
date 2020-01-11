883cf2383000418a509bf9752a398123205e2c9a
Nikita <itomatpast@gmail.com>
2020-01-12 01:27:24 +0200

    added functional post

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string
+  text: string
+  id?: number
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angularApp';
+ posts: Post[] = [
+   {title: 'Хочу выучить Angular components', text: 'Я все еще учу компоненты', id: 1},
+   {title: 'Следующий блок ',text:'Будет про директивы и еще про пайпы', id:2}
+ ]
+
+ updatePosts(post:Post){
+   console.log('Post ', post)
+   this.posts.unshift(post);
+ }
 }
+
+
